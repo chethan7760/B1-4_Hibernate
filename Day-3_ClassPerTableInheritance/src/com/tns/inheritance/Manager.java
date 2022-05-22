9e50a915855dc7bd63c124271d4357780e651104
CHETHAN R <91748752+chethan7760@users.noreply.github.com>
2022-05-22 22:16:24 +0530

    First commit

---
+++ Day-3_ClassPerTableInheritance/src/com/tns/inheritance/Manager.java
@@ -0,0 +1,23 @@
+package com.tns.inheritance;
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_store3")
+public class Manager extends Employee 
+{
+	private static final long serialVersionUID=1L;
+	private String deptName;
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+
+}
---
+++ Day-3_JoinedTableInheritance/src/com/tns/inheritance/Manager.java
@@ -0,0 +1,25 @@
+package com.tns.inheritance;
+
+
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_store4")
+public class Manager extends Employee 
+{
+	private static final long serialVersionUID=1L;
+	private String deptName;
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+
+}
